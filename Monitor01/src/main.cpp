66e6b34ae86cfb4a2f207fafb0c256de8d60c59f
israeljaimeshernandez <54376054+israeljaimeshernandez@users.noreply.github.com>
2025-11-23 09:52:49 -0600

    Fix portal button escaping and update legend

--- Monitor01/src/main.cpp
+++ Monitor01/src/main.cpp
@@ -1,3 +1,4 @@
+// 22 - Corrección: escapar atributos onclick con comillas y evitar errores de compilación en botones del portal
 // 21 - Corrección: escapar las comillas de botones HTML para compilar sin errores y mantener las acciones del portal
 // 20 - Corrección: procesar los escaneos WiFi incluso con el portal abierto para que el refresco muestre redes reales
 // 19 - Corrección: permitir que el reescaneo WiFi concluya y refresque la lista en el portal
@@ -1203,11 +1204,12 @@ void procesarEscaneoRedes() {
     int idx = indices[i];
     String ssid = WiFi.SSID(idx);
     int rssi = WiFi.RSSI(idx);
+    String safeSsid = escapeForJS(ssid);
 
     scannedNetworks += "<div class='network-item'>";
     scannedNetworks += "<label>" + ssid + "</label>";
     scannedNetworks += "<span class='signal'>" + String(rssi) + " dBm</span>";
-    scannedNetworks += "<button type='button' onclick=\"prefillNetwork('" + ssid + "')\"">Usar</button>";
+    scannedNetworks += "<button type='button' onclick=\"prefillNetwork('" + safeSsid + "')\"">Usar</button>";
     scannedNetworks += "</div>";
   }
 
@@ -1276,7 +1278,7 @@ void handleRoot() {
       }
       devicesList += "<div class='network-item device-item'>";
       devicesList += "<div class='device-info'><strong>" + nombre + "</strong><br><small>MAC: " + mac + "</small></div>";
-      devicesList += "<button type='button' onclick=\"deleteDevice('" + mac + "')\"">Eliminar</button>";
+      devicesList += "<button type='button' onclick=\"deleteDevice('" + escapeForJS(mac) + "')\"">Eliminar</button>";
       devicesList += "</div>";
     }
   }
